4ff96d12402f2fb2b3ea890f69510dee7b8ef7e4
lupersuki <zebralova1337@gmail.com>
2024-04-24 13:10:40 +0200

    add function count_letters

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text = get_book_text(book_path)
     print(text)
     print(f"Word count: {count_words(text)}")
+    print(count_letters(text))
 
 
 def get_book_text(path):
@@ -12,5 +13,14 @@ def get_book_text(path):
 def count_words(text):
     words = (len(text.split()))
     return words
+
+def count_letters(text):
+    letters_dict = {}
+    for letter in text:
+        lowered = letter.lower()
+        if lowered not in letters_dict:
+            letters_dict[lowered] = 1
+        letters_dict[lowered] += 1
+    return letters_dict
 
 main()
