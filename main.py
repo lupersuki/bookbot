9726f25beccb874abc4c957ebdc44aa2968e0a95
lupersuki <zebralova1337@gmail.com>
2024-04-24 12:28:39 +0200

    add functions get_book_text and count_words

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+    print(f"Word count: {count_words(text)}")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(text):
+    words = (len(text.split()))
+    return words
+
+main()
