807fe07158f03ffde3db92b6b080779d59509d0d
lupersuki <zebralova1337@gmail.com>
2024-04-24 16:16:47 +0200

    add function sorting and removing non alphanumerics

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
-    print(f"Word count: {count_words(text)}")
-    print(count_letters(text))
+    letter_count = count_letters(text)
+    sorted_count = sorted(letter_count.items(), key=lambda item: item[1], reverse=True)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{count_words(text)} words found in the document")
+    for letter, count in sorted_count:
+        if letter.isalpha():
+            print(f"The '{letter}' character was found {count} times")
+
+    print("--- End report ---")
 
 
 def get_book_text(path):
@@ -22,5 +29,8 @@ def count_letters(text):
             letters_dict[lowered] = 1
         letters_dict[lowered] += 1
     return letters_dict
+
+def sort_on(dict):
+    return dict["num"]
 
 main()
